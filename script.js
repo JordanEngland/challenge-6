3c47e85358dbc3bd7ab4e7489f565a137d816511
JordanEngland <lcplengland@gmail.com>
2023-04-26 12:15:01 -0500

    javascript to update current weather and weather icon to change when new city is added

--- script.js
+++ script.js
@@ -8,3 +8,51 @@ document.addEventListener('DOMContentLoaded', function() {
   setInterval(updateCurrentDateTime, 1000);
 });
 
+const apiKey = "e6045ed476404c7301fa015e7d168a41";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+  const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
+  const data = await response.json();
+
+  console.log(data);
+
+  document.querySelector('.city').innerHTML = data.name;
+  document.querySelector('.temperature').innerHTML = Math.round(data.main.temp) + "°F";
+  document.querySelector('.humidity').innerHTML = data.main.humidity + "%" ;
+  document.querySelector('.wind').innerHTML = data.wind.speed + "mph";
+
+  if(data.weather[0].main == "Clouds"){
+    weatherIcon.src = "images/clouds.png";
+  }
+  else if(data.weather[0].main == "Clear"){
+    weatherIcon.src = "images/clear.png";
+  }
+  else if(data.weather[0].main == "Drizzle"){
+    weatherIcon.src = "images/drizzle.png";
+  }
+  else if(data.weather[0].main == "Humidity"){
+    weatherIcon.src = "images/humidity.png";
+  }
+  else if(data.weather[0].main == "Mist"){
+    weatherIcon.src = "images/mist.png";
+  }
+  else if(data.weather[0].main == "Rain"){
+    weatherIcon.src = "images/rain.png";
+  }
+  else if(data.weather[0].main == "Snow"){
+    weatherIcon.src = "images/snow.png";
+  }
+  else if(data.weather[0].main == "Wind"){
+    weatherIcon.src = "images/wind.png";
+  }
+}
+
+searchBtn.addEventListener("click", ()=>{
+  checkWeather(searchBox.value);
+})
+
